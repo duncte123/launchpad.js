1878bb2d37afe1e38b233bfefa810c4896e543da
Rico Huijbers <rix0rrr@gmail.com>
2022-05-17 19:09:01 +0200

    Add missing file

---
+++ src/internal/utils.ts
@@ -0,0 +1,71 @@
+import midi from 'midi';
+
+export const NORMAL_NOTE = 144;
+export const CONTROL_NOTE = 176;
+
+export function onExit(fn: () => void): void {
+  let manualExit = false;
+  const doExit = makeExitHandler(true);
+
+  process.on('exit', makeExitHandler());
+  process.on('SIGINT', doExit);
+  process.on('uncaughtException', (e) => {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    doExit();
+  });
+
+  // https://github.com/remy/nodemon#controlling-shutdown-of-your-script
+  process.once('SIGUSR2', () => {
+    fn();
+    process.kill(process.pid, 'SIGUSR2');
+  });
+
+  function makeExitHandler(exit: boolean = false) {
+    return (): void => {
+      if (!manualExit) {
+        process.stdout.write('\n');
+        fn();
+      }
+
+      if (exit) {
+        manualExit = true;
+        process.exit();
+      }
+    };
+  }
+}
+
+export function allDeviceNames(chan: midi.Input): string[] {
+  return range(chan.getPortCount()).map(i => chan.getPortName(i));
+}
+
+export function findDevice(regex: RegExp, midiInput: midi.Input): number {
+  const ports = allDeviceNames(midiInput);
+  const index = ports.findIndex(p => p.match(regex));
+
+  if (index === -1) {
+    throw new Error(`No MIDI device matches '${regex}'. Found: ${ports.join(', ') || '(none)'}`);
+  }
+
+  return index;
+}
+
+
+/**
+ * Return the numbers [0..n-1]
+ */
+export function range(n: number): number[] {
+  const ret = [];
+  for (let i = 0; i < n; i++) {
+    ret.push(i);
+  }
+  return ret;
+}
+
+/**
+ * Return all (x, y) coordinates
+ */
+export function allXy(width: number, height: number): Array<[number, number]> {
+  return range(height).flatMap(y => range(width).map(x => [x, y] as [number, number]));
+}
